c95d63657a378cb7fd170d3210ebc241ebf23d2e
Franktronics <franktronicsprog@gmail.com>
2025-12-21 20:28:23 +0100

    fix networt interface state

--- packages/front/src/pages/home/stores/scan-control.context.tsx
+++ packages/front/src/pages/home/stores/scan-control.context.tsx
@@ -1,5 +1,5 @@
 import { createContext, useCallback, useContext, useState } from 'react'
-import type { ComponentPropsWithoutRef } from 'react'
+import type { ComponentPropsWithoutRef, Dispatch, SetStateAction } from 'react'
 import { wsFetcher, fetcher } from '../../../config/client-trpc'
 import { ClientErrorParser, useMutateFetcher } from '@repo/utils'
 import { toast } from '@repo/ui/atoms'
@@ -17,8 +17,8 @@ export type ScanControlContextType = {
     captureStatus: CAPTURE_STATUS
     changeCaptureStatus: (capturing: CAPTURE_STATUS) => void
     rawPackets: { parsed: any; raw: any; id: number }[]
-    interf: ContextNetinterface | null
-    setInterface: (interf: ContextNetinterface | null) => void
+    interf: ContextNetinterface
+    setInterface: Dispatch<SetStateAction<ContextNetinterface>>
 }
 
 export type ContextNetinterface = {
@@ -40,7 +40,7 @@ type ScanControlProviderProps = {} & ComponentPropsWithoutRef<'div'>
 export const ScanControlProvider = (props: ScanControlProviderProps) => {
     const { children, ...rest } = props
     const [captureStatus, setCaptureStatus] = useState<CAPTURE_STATUS>(CAPTURE_STATUS.IDLE)
-    const [interf, setInterface] = useState<ContextNetinterface | null>(null)
+    const [interf, setInterface] = useState<ContextNetinterface>({ name: '', infos: [] })
 
     const [rawPackets, setRawPackets] = useState<{ parsed: any; raw: any; id: number }[]>([])
     const { mutateData: stopScan } = useMutateFetcher({
@@ -52,28 +52,35 @@ export const ScanControlProvider = (props: ScanControlProviderProps) => {
         popupOnError: true,
     })
 
-    const handleChangeCaptureStatus = useCallback(async (status: CAPTURE_STATUS) => {
-        if (status === CAPTURE_STATUS.IDLE) {
-            await stopScan()
-            setCaptureStatus(CAPTURE_STATUS.IDLE)
-        } else if (status === CAPTURE_STATUS.INNITIALIZING) {
-            wsFetcher.scan.start.handle(
-                { interface: interf?.name || '' },
-                {
-                    onmessage: (data) => {
-                        console.log('Received from ws echo:', data)
+    const handleChangeCaptureStatus = useCallback(
+        async (status: CAPTURE_STATUS) => {
+            if (interf.name === '') {
+                toast.warning('Select an interface')
+                return
+            }
+            if (status === CAPTURE_STATUS.IDLE) {
+                await stopScan()
+                setCaptureStatus(CAPTURE_STATUS.IDLE)
+            } else if (status === CAPTURE_STATUS.INNITIALIZING) {
+                wsFetcher.scan.start.handle(
+                    { interface: interf.name },
+                    {
+                        onmessage: (data) => {
+                            console.log('Received from ws echo:', data)
+                        },
+                        onerror: (error) => {
+                            setCaptureStatus(CAPTURE_STATUS.ERROR)
+                            toast.error(<ClientErrorParser error={error} />, {
+                                duration: 5000,
+                            })
+                        },
                     },
-                    onerror: (error) => {
-                        setCaptureStatus(CAPTURE_STATUS.ERROR)
-                        toast.error(<ClientErrorParser error={error} />, {
-                            duration: 5000,
-                        })
-                    },
-                },
-            )
-            setCaptureStatus(CAPTURE_STATUS.CAPTURING)
-        }
-    }, [])
+                )
+                setCaptureStatus(CAPTURE_STATUS.CAPTURING)
+            }
+        },
+        [interf.name],
+    )
 
     const value: ScanControlContextType = {
         captureStatus,
