48bfe74722bd8108a8024b9a28fee560773193d2
franktronicsprog <franktronicsprog@gmail.com>
2026-01-25 20:38:19 +0100

    feat: add delete functionality to node layout menu; integrate duplicate option with icons

--- packages/ui/src/atomic/organisms/analysis-graph/nodes-layout/node-layout-menu.tsx
+++ packages/ui/src/atomic/organisms/analysis-graph/nodes-layout/node-layout-menu.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps } from 'react'
+import { ComponentProps, useCallback } from 'react'
 import {
     ContextMenuContent,
     ContextMenuGroup,
@@ -7,26 +7,59 @@ import {
 } from '../../../atoms/context-menu'
 import { useNodeContext } from './node-layout-context'
 import { useReactFlow } from '@xyflow/react'
+import { Layers2, Package, Pencil, Trash } from 'lucide-react'
 
 type NodeLayoutMenuProps = {} & ComponentProps<typeof ContextMenuContent>
 const Menu = (props: NodeLayoutMenuProps) => {
     const { children, className, ...rest } = props
     const { setRenamePopupOpen, setPopupOpen, nodeId } = useNodeContext()
-    const { setNodes } = useReactFlow()
+    const { setNodes, setEdges, getNode, addNodes } = useReactFlow()
 
-    const handleDeleteBtnClick = () => {
+    const handleDeleteBtnClick = useCallback(() => {
         setNodes((nds) => nds.filter((n) => n.id !== nodeId))
-    }
+        setEdges((eds) => eds.filter((e) => e.source !== nodeId && e.target !== nodeId))
+    }, [setNodes, setEdges, nodeId])
+    const handleDuplicateBtn = useCallback(() => {
+        const node = getNode(nodeId)
+        if (!node) return
+        const position = {
+            x: node.position.x,
+            y: node.position.y + (node.measured?.height ?? 0) + 20,
+        }
+        addNodes({
+            ...node,
+            selected: false,
+            dragging: false,
+            id: `${node.id}-copy-${Date.now()}`,
+            position,
+        })
+    }, [getNode, addNodes, nodeId])
 
     return (
-        <ContextMenuContent className="w-48" {...rest}>
+        <ContextMenuContent className="w-52" {...rest}>
             <ContextMenuGroup>
-                <ContextMenuItem onClick={() => setPopupOpen(true)}>Open</ContextMenuItem>
-                <ContextMenuItem onClick={() => setRenamePopupOpen(true)}>Rename</ContextMenuItem>
+                <ContextMenuItem onClick={() => setPopupOpen(true)}>
+                    <Package />
+                    Open
+                </ContextMenuItem>
+                <ContextMenuItem onClick={() => setRenamePopupOpen(true)}>
+                    <Pencil />
+                    Rename
+                </ContextMenuItem>
             </ContextMenuGroup>
             <ContextMenuSeparator />
             <ContextMenuGroup>
-                <ContextMenuItem onClick={handleDeleteBtnClick}>Delete</ContextMenuItem>
+                <ContextMenuItem onClick={handleDuplicateBtn}>
+                    <Layers2 />
+                    Duplicate
+                </ContextMenuItem>
+            </ContextMenuGroup>
+            <ContextMenuSeparator />
+            <ContextMenuGroup>
+                <ContextMenuItem variant="destructive" onClick={handleDeleteBtnClick}>
+                    <Trash />
+                    Delete
+                </ContextMenuItem>
             </ContextMenuGroup>
             {children}
         </ContextMenuContent>
